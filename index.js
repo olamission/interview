2182adbe6df7a00b2d50fe047f399b87cc8d4ef0
ola <Oayodele@hotmail.co.uk>
2022-09-28 21:08:53 +0100

    inteview

---
+++ index.js
@@ -0,0 +1,44 @@
+//examples of bike serial nubers
+const bikeString1 = '6F2k-1d-0-z'
+const bikeString2 = '1-4k-9-b'
+
+//function that changes the code to a valid format
+const format_serial_number = (bikeString , length) => {
+  /*
+  value 1 is the final result by reversing 2 
+  value 2 is when we add the new dashes to the reversed string 3
+  value 3 is when we take out the previous dashes from the bikeString
+  value 4 counter is a way I can count the index of the string
+  */
+  newSerialNo = '';//1
+  newBikeSerialNo = '';//2
+  undashedSerialNo = '';//3
+  counter = 0;//4
+  //gets rid of hashes
+  for (let i=0 ; i<bikeString.length ; i++) {
+    undashedSerialNo = bikeString[i]!= '-' ? undashedSerialNo += bikeString[i] : undashedSerialNo ;
+    /*if (bikeString[i]!= '-') { undashedSerialNo += bikeString[i] 
+    }*/
+  } 
+
+
+  // reverses string and adds hashes , implements format system
+  for (let i=undashedSerialNo.length-1 ; i>=0 ; i--) {
+    /*if ((counter)%length == 0 && counter!= 0) {
+      newBikeSerialNo += '-'
+    }*/
+    newBikeSerialNo = counter%length == 0 && counter!= 0 ? newBikeSerialNo += '-' : newBikeSerialNo ;
+    newBikeSerialNo += undashedSerialNo[i]
+    counter++
+  }
+
+  //reverses string back again and converts to upper case
+  for (var i = newBikeSerialNo.length - 1; i >= 0; i--) { 
+        newSerialNo += newBikeSerialNo[i];
+    }
+    return newSerialNo.toUpperCase();
+}
+
+console.log([format_serial_number(bikeString1,4) , format_serial_number(bikeString2,2)])
+console.log( format_serial_number('1ggg-4kgg-r-b555' , 5))
+
